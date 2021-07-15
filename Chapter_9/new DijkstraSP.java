a0ed33c281a88e9dbf42197fc722edb6f8a36c1c
Youngseo Yoo <7dudtj@naver.com>
2021-07-15 11:54:40 +0900

    Update new DijkstraSP.java

--- Chapter_9/new DijkstraSP.java
+++ Chapter_9/new DijkstraSP.java
@@ -6,8 +6,6 @@ N : 정점의 수
 
 모든 정점이 연결되어있는지 여부에 상관없이 사용 가능.
 PriorityQueue를 사용하여, 시간복잡도를 O(N^2)에서 O(NlogN)으로 낮춤
-64번째줄에 'N-1' 자리에는 도착점(endPoint)를 넣어주면 됨
-64번째 줄은 필요에 맞게 사용할 것. 사용하지 않아도 
 */
 
 import java.util.List;
@@ -51,18 +49,16 @@ public class Main {
 
     // do Dijkstra
     distance[startPoint] = 0;
-    //visited[startPoint] = true; // 안써도 될 듯
     PriorityQueue<Edge> pq = new PriorityQueue<>(new Comparator<Edge>(){
       @Override
       public int compare(Edge e1, Edge e2){
         return e1.weight - e2.weight;
       }
     });
-    pq.add(new Edge(startPoint, 0)); // 수정된 부분. 0 대신 startPoint를 넣어줘야함
+    pq.add(new Edge(startPoint, 0));
     while (!pq.isEmpty()){
       Edge temp = pq.poll();
-      if (visited[temp.adjvertex]) continue; // 이 조건이 모든 경우에서 작동하는 듯
-      //if (temp.adjvertex == N-1) continue; // N-1: endPoint
+      if (visited[temp.adjvertex]) continue;
       visited[temp.adjvertex] = true;
       for (Edge e: adjList[temp.adjvertex]){
         if (!visited[e.adjvertex] && distance[temp.adjvertex] + e.weight < distance[e.adjvertex]){
